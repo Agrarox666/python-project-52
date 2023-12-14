05c6160c2da1872b198574864568dbee09621e64
Igor Loskutov <omfi017@gmail.com>
2023-12-14 23:38:04 +0500

    db bugs

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -o errexit
 
-poetry install && psql PGPASSWORD=DakNkMWU4tAY9dxFVKbNmKRuaa1yMdc2 psql -h dpg-clst94tcm5oc73bbmu90-a.oregon-postgres.render.com -U test_manager_db_user test_manager_db
+poetry install && psql -h dpg-clst94tcm5oc73bbmu90-a.oregon-postgres.render.com -U test_manager_db_user test_manager_db
 python3 manage.py makemigrations
 python3 manage.py migrate
