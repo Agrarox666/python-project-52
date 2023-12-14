3f8ee4946d971c8fc22e896f0c17a4058f7b5bf3
Igor Loskutov <omfi017@gmail.com>
2023-12-15 00:49:06 +0500

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 poetry install
+python manage.py makemigrations task_manager
+python manage.py makemigrations statuses
+python manage.py makemigrations labels
+python manage.py makemigrations tasks
 python manage.py migrate
