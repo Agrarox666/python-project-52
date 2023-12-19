e8268d7effc6f62d2bfd5c08d6bb73d62a10f54e
Igor Loskutov <omfi017@gmail.com>
2023-12-19 20:53:08 +0500

    fix bugs

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
@@ -10,19 +10,24 @@ class Task(models.Model):
     executor = models.ForeignKey(get_user_model(),
                                  on_delete=models.PROTECT,
                                  related_name='task_executor',
+                                 verbose_name=_('Executor'),
                                  blank=True,
                                  null=True)
     author = models.ForeignKey(get_user_model(),
                                on_delete=models.PROTECT,
-                               related_name='task_author', )
+                               related_name='task_author',
+                               verbose_name=_('Author'))
     status = models.ForeignKey(Status,
                                on_delete=models.PROTECT,
                                related_name='task_status',
+                               verbose_name=_('Status'),
                                null=True)
-    labels = models.ManyToManyField(Label, blank=True)
+    labels = models.ManyToManyField(Label,
+                                    blank=True,
+                                    verbose_name=_('Labels'))
 
-    name = models.CharField(max_length=150, unique=True)
-    description = models.TextField(default=None, blank=True)
+    name = models.CharField(_('Name'), max_length=150, unique=True)
+    description = models.TextField(_('Description'), default=None, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
