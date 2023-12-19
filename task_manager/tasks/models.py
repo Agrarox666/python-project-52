2122dee1cd9bbc23ba781760b74f41d3402d5d0f
Igor Loskutov <omfi017@gmail.com>
2023-12-19 22:01:32 +0500

    fix bugs

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
@@ -8,26 +7,21 @@ from task_manager.statuses.models import Status
 
 class Task(models.Model):
     executor = models.ForeignKey(get_user_model(),
-                                 on_delete=models.PROTECT,
+                                 on_delete=models.SET_DEFAULT,
                                  related_name='task_executor',
-                                 verbose_name=_('Executor'),
-                                 blank=True,
-                                 null=True)
+                                 default=None, )
     author = models.ForeignKey(get_user_model(),
-                               on_delete=models.PROTECT,
+                               on_delete=models.SET_DEFAULT,
                                related_name='task_author',
-                               verbose_name=_('Author'))
+                               default=None)
     status = models.ForeignKey(Status,
-                               on_delete=models.PROTECT,
+                               on_delete=models.SET_DEFAULT,
                                related_name='task_status',
-                               verbose_name=_('Status'),
-                               null=True)
-    labels = models.ManyToManyField(Label,
-                                    blank=True,
-                                    verbose_name=_('Labels'))
+                               default=None)
+    labels = models.ManyToManyField(Label, default=None)
 
-    name = models.CharField(_('Name'), max_length=150, unique=True)
-    description = models.TextField(_('Description'), default=None, blank=True)
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField(default=None)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
