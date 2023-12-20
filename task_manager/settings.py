67d95ec748071df91e082f4e9df82ef798542e00
Igor Loskutov <omfi017@gmail.com>
2023-12-20 22:53:44 +0500

    correct mentor's comments

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
@@ -82,18 +81,17 @@ TEMPLATES = [
     },
 ]
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "users_data_base"),
-        }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
 }
 if os.getenv("DEVELOPMENT_MODE"):
     DATABASES.update(
         {
-            "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": os.path.join(BASE_DIR, "users_data_base"),
+            }
         }
     )
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
